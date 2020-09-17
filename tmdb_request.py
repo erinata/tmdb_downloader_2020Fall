235da29222bd9e065940c014908afcea266291c6
Chungsang Tom Lam <erinata@gmail.com>
2020-09-17 13:45:51 -0400

    make folder

--- tmdb_request.py
+++ tmdb_request.py
@@ -6,11 +6,20 @@ api_key = sys.argv[1]
 
 # print(api_key)
 
-response = urllib.request.urlopen('https://api.themoviedb.org/3/movie/latest?api_key=' + api_key)
-json_response = json.load(response)
+# response = urllib.request.urlopen('https://api.themoviedb.org/3/movie/latest?api_key=' + api_key)
+# json_response = json.load(response)
 
-movie_count = int(json_response['id'])
-print(movie_count)
+# movie_count = int(json_response['id'])
+# print(movie_count)
+
+# movie_start = movie_count-5
+
+
+movie_id = '550'
+response = urllib.request.urlopen('https://api.themoviedb.org/3/movie/' + movie_id + '?api_key=' + api_key)
+json_response = json.load(response)
+print(json_response)
+
 
 
 
