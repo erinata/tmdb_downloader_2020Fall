5b18369feae51b59d7bcd2040e9c37ed90b76c8b
Chungsang Tom Lam <erinata@gmail.com>
2020-09-24 13:38:17 -0400

    request and parse

--- tmdb_request.py
+++ tmdb_request.py
@@ -1,24 +1,38 @@
 import urllib.request
 import json
 import sys
+import os
+import time
 
 api_key = sys.argv[1]
 
+if not os.path.exists('json_files'):
+	os.mkdir('json_files')
+
 # print(api_key)
 
-# response = urllib.request.urlopen('https://api.themoviedb.org/3/movie/latest?api_key=' + api_key)
-# json_response = json.load(response)
+response = urllib.request.urlopen('http://api.themoviedb.org/3/movie/latest?api_key=' + api_key)
+json_response = json.load(response)
 
-# movie_count = int(json_response['id'])
+movie_count = int(json_response['id'])
 # print(movie_count)
 
-# movie_start = movie_count-5
+movie_start = movie_count-5 
+# movie_start = 1
+
+for i in range(movie_start,movie_count):
+	print(i)
+	movie_id = str(i)
+	response = urllib.request.urlopen('http://api.themoviedb.org/3/movie/' + movie_id + '?api_key=' + api_key)
+	json_response = json.load(response)
+	# print(json_response)
+	f = open("json_files/tmdb" + movie_id + ".json", "w")
+	f.write(json.dumps(json_response))
+	f.close()
+	time.sleep(15)
 
 
-movie_id = '550'
-response = urllib.request.urlopen('https://api.themoviedb.org/3/movie/' + movie_id + '?api_key=' + api_key)
-json_response = json.load(response)
-print(json_response)
+
 
 
 
